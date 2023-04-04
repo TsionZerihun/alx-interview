e64212dd369cbb0b625b2e41172cd277e10c5e8d
TsionZerihun <TsionZelekeZerihun@gmail.com>
2023-04-04 22:38:10 +0300

    temp variable

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 """
-0-pascal_triangle
+task 0: returns a list of lists of ints
 """
 
 
 def pascal_triangle(n):
     """
-    Returns a list of integers
-    representing the Pascal Triangle of n
-    returns empty list if n <= 0
+    Returns a list of ints
     """
     k = []
     if n <= 0:
         return k
     k = [[1]]
     for i in range(1, n):
-        temp = [1]
+        triangle = [1]
         for j in range(len(k[i - 1]) - 1):
             curr = k[i - 1]
-            temp.append(k[i - 1][j] + k[i - 1][j + 1])
-        temp.append(1)
-        k.append(temp)
+            triangle.append(k[i - 1][j] + k[i - 1][j + 1])
+        triangle.append(1)
+        k.append(triangle)
     return k
