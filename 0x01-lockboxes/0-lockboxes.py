a0e330b1657db75e29fa532aebdb564b4cf24e3a
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-04-05 12:30:50 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Lockboxes """
+""" task 0 """
 
 
 def canUnlockAll(boxes):
-    """ LockBoxes Function """
+    """ Returns boolean based on boxes """
     T = []
     for i in range(1, len(boxes)):
         order = [T.extend(x) for x in boxes[:i] + boxes[i + 1:]]
