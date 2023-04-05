148aa40c47d937d8936bfed2a608cf09e411fcc2
TsionZerihun <TsionZelekeZerihun@gmail.com>
2023-04-05 12:41:41 +0300

    task0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-""" Task0"""
+"""
+Task0
+"""
 
 
 def minOperations(n):
-    """
-    Returns ops
-    """
-    # all outputs should be at least 2 char: (min, Copy All => Paste)
-    if (n < 2):
-        return 0
-    ops, root = 0, 2
-    while root <= n:
-        # if n evenly divides by root
-        if n % root == 0:
-            # total even-divisions by root = total operations
-            ops += root
-            # set n to the remainder
-            n = n / root
-            # reduce root to find remaining smaller vals that evenly-divide n
-            root -= 1
-        # increment root until it evenly-divides n
-        root += 1
-    return ops
+    """returns t"""
+    t = 0
+    m = 2
+    while n > 1:
+        while not n % m:
+            t += m
+            n /= m
+        m += 1
+    return t
