35b661aa390c05889f358405918ae8984c707497
TsionZerihun <TsionZelekeZerihun@gmail.com>
2023-04-05 12:12:06 +0300

    add comment

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """
-task0:canUnlockAll
+task0: canUnlockAll
 """
 
 
 def canUnlockAll(boxes):
     """
-    Returns boolean
+    return boolean bases on key
     """
-    keys = set()
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
 
-    for box_id, box in enumerate(boxes):
-        if len(box) == 0 or box_id == 0:
-            keys.add(box_id)
-        for key in box:
-            if key < len(boxes) and key != box_id:
-                keys.add(key)
-        if len(keys) == len(boxes):
-            return True
-    return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
