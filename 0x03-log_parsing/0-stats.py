ecfe84af542afa3edec90bd4bf3925986d418bbb
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-04-05 12:57:04 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,10 +5,6 @@ import sys
 
 def print_msg(dict_sc, total_file_size):
     """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
     Returns:
         Nothing
     """
