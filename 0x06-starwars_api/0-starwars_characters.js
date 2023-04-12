583611b3c3bd05185cfabfad5d17c26bc73ee9a3
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-04-12 10:13:15 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,40 @@
 #!/usr/bin/node
+/**
+ * Script for printing out all Star Wars Characters
+ */
+
 const request = require('request');
-const movieId = process.argv[2];
-const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
-  method: 'GET'
-};
-
-request(options, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
+const id = process.argv[2];
+if (!id || isNaN(id)) {
+  process.exit(1);
 }
+
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const responses = [];
+
+  const json = JSON.parse(body);
+  const characters = json.characters;
+
+  characters.forEach((character) => {
+    const url = character;
+    const promise = new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(JSON.parse(body).name);
+      });
+    });
+    responses.push(promise);
+  });
+  Promise.all(responses)
+    .then(names => console.log(names.join('\n')))
+    .catch(errors => console.log(errors));
+});
